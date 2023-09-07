b3c631a478a73caa44db7ec88c40ee4f402ed892
devvagner <wagnerluizsg@outlook.com.br>
2023-09-07 10:52:46 -0300

    :bug: fix: add cors options

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,9 @@ const port = process.env.PORT || 3003;
 const app = express();
 
 app.use( express.json() );
-app.use( cors() );
+app.use( cors());
+app.options( '*', cors() );
+
 
 const server = app.listen( port, () => {
     if ( server ) {
